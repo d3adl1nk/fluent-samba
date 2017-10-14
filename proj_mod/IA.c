6d0211f80334b49ed2651340a7d33267542315a3
Clement Legrand <clegrand@caraja.inrialpes.fr>
2017-10-14 15:04:05 +0200

    uniformisation
    shorter indentation -> 80 characters/line

--- proj_mod/IA.c
+++ proj_mod/IA.c
@@ -5,21 +5,21 @@
 //TODO: Needs to change -> same model as update_board
 int calc_new_cells(board_d* board, char player, char color)
 {
-	int num_new_cells = 0;
-	
-	int i, j;
-	for (i = 0; i < BOARD_SIZE; i++)
+  int num_new_cells = 0;
+  
+  int i, j;
+  for (i = 0; i < BOARD_SIZE; i++)
+    {
+      for (j = 0; j < BOARD_SIZE; j++)
 	{
-		for (j = 0; j < BOARD_SIZE; j++)
-		{
-			if (is_next_to_player(board, player, i, j) && get_cell(board, i, j) == color)
-			{
-				num_new_cells++;
-			}
-		}
+	  if (is_next_to_player(board, player, i, j) &&
+	      get_cell(board, i, j) == color)
+	    {
+	      num_new_cells++;
+	    }
 	}
-	
-	return num_new_cells;
+    }
+  return num_new_cells;
 }
 
 int calc_new_cells_rec(board_d* board, board_d* board_visited, char player, char color, int i, int j)
@@ -96,28 +96,27 @@ int calc_new_cells_optimized(board_d* board, char player, char color)
 
 char IA_greedy(board_d* board)
 {
-	char color, max_color;
-	int temp_cells;
-	int max_cells = 0;
-	
-	for (color = 65; color < 72; color++)
+  char color, max_color;
+  int temp_cells;
+  int max_cells = 0;
+  
+  for (color = 65; color < 72; color++)
+    {
+      temp_cells = calc_new_cells_optimized(board, 'v', color);
+      if (temp_cells > max_cells)
 	{
-		temp_cells = calc_new_cells_optimized(board, 'v', color);
-		if (temp_cells > max_cells)
-		{
-			max_cells = temp_cells;
-			max_color = color;
-		}
+	  max_cells = temp_cells;
+	  max_color = color;
 	}
-	
-	return max_color;
+    }
+   return max_color;
 }
 
 /** The IA chooses a random color*/
 char IA_random(void)
 {
-	char rand_color = 65 + (rand() % 7);
-	return rand_color;
+  char rand_color = 65 + (rand() % 7);
+  return rand_color;
 }
 
 int border(board_d* board, char player)
