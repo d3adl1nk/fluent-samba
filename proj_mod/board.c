36526ed26d24bf4c926fcfb36f6aab1712ab0e37
Clement Legrand <clegrand@caraja.inrialpes.fr>
2017-10-14 14:59:40 +0200

    uniformisation of the code
    using (i,j) coordonates instead of (x,y)
    + shorter indentation -> 80 characters / line

--- proj_mod/board.c
+++ proj_mod/board.c
@@ -4,26 +4,26 @@
 
 board_d* board_create(void)
 {	
-	//char cells[BOARD_SIZE * BOARD_SIZE] = { 0 };
-	//board_d init_board = {{ 0 }, 1, 1};
-	//board_d* board = &init_board;
-	
-	board_d* board = malloc(sizeof(board_d));
-	(*board).num_cells_up = 0;
-	(*board).num_cells_down = 0;
-	
-	return board;
+  //char cells[BOARD_SIZE * BOARD_SIZE] = { 0 };
+  //board_d init_board = {{ 0 }, 1, 1};
+  //board_d* board = &init_board;
+  
+  board_d* board = malloc(sizeof(board_d));
+  (*board).num_cells_up = 0;
+  (*board).num_cells_down = 0;
+  
+  return board;
 }
 
 void board_free(board_d* board)
 {
-	free(board);
+  free(board);
 }
 
 /** Retrieves the color of a given board cell */
-char get_cell(board_d* board, int x, int y)
+char get_cell(board_d* board, int i, int j)
 {
-    return (*board).cells[y * BOARD_SIZE + x];
+  return (*board).cells[i * BOARD_SIZE + j];
 }
 
 int get_num_cells_up(board_d* board)
@@ -37,18 +37,18 @@ int get_num_cells_down(board_d* board)
 }
 
 /** Changes the color of a given board cell */
-void set_cell(board_d* board, int x, int y, char color)
+void set_cell(board_d* board, int i, int j, char color)
 {
-  if (get_cell(board, x, y) == 'v')
+  if (get_cell(board, i, j) == 'v')
     {
       (*board).num_cells_down -= 1;
     }
-  if (get_cell(board, x, y) == '^')
+  if (get_cell(board, i, j) == '^')
     {
       (*board).num_cells_up -= 1;
     }
   
-  (*board).cells[y * BOARD_SIZE + x] = color;
+  (*board).cells[i * BOARD_SIZE + j] = color;
   if (color == 'v')
     {
       (*board).num_cells_down += 1;
@@ -67,52 +67,52 @@ void set_cell(board_d* board, int x, int y, char color)
  */
 void print_board(board_d* board)
 {
-	printf("\n");
+  printf("\n");
 	
-    int i, j;
-    for (i = 0; i < BOARD_SIZE; i++) {
-        for (j = 0; j < BOARD_SIZE; j++) {
-            printf("%c ", get_cell(board, i, j));
-        }
-        printf("\n");
+  int i, j;
+  for (i = 0; i < BOARD_SIZE; i++) {
+    for (j = 0; j < BOARD_SIZE; j++) {
+      printf("%c ", get_cell(board, i, j));
     }
     printf("\n");
+  }
+  printf("\n");
 }
 
 /** Creates a random starting board, with a player on two opposite corners */
 void rand_board(board_d* board)
 {
-	srand(time(NULL));
-	char rand_color;
-	
-	int i, j;
-	for (i = 0; i < BOARD_SIZE; i++)
+  srand(time(NULL));
+  char rand_color;
+  
+  int i, j;
+  for (i = 0; i < BOARD_SIZE; i++)
+    {
+      for (j = 0; j < BOARD_SIZE; j++)
 	{
-		for (j = 0; j < BOARD_SIZE; j++)
-		{
-			rand_color = 65 + (rand() % 7);
-			set_cell(board, i, j, rand_color);
-		}
+	  rand_color = 65 + (rand() % 7);
+	  set_cell(board, i, j, rand_color);
 	}
-	
-	//initial state of both players
-	set_cell(board, BOARD_SIZE - 1, 0, 'v');
-	set_cell(board, 0, BOARD_SIZE - 1, '^');
+    }
+  
+  //initial state of both players
+  set_cell(board, BOARD_SIZE - 1, 0, 'v');
+  set_cell(board, 0, BOARD_SIZE - 1, '^');
 }
 
 /** Tells if a cell is inside the board */
-bool is_inb(int x, int y)
+bool is_inb(int i, int j)
 {
-	return ((x >= 0 && x < BOARD_SIZE) && (y >= 0 && y < BOARD_SIZE));
+  return ((i >= 0 && i < BOARD_SIZE) && (j >= 0 && j < BOARD_SIZE));
 }
 
 /** Tells if a cell is next to a given player*/
-bool is_next_to_player(board_d* board, char player, int x, int y)
+bool is_next_to_player(board_d* board, char player, int i, int j)
 {
-	return ((is_inb(x-1, y) && (get_cell(board, x-1, y) == player)) ||
-			(is_inb(x+1, y) && (get_cell(board, x+1, y) == player)) ||
-			(is_inb(x, y-1) && (get_cell(board, x, y-1) == player)) ||
-			(is_inb(x, y+1) && (get_cell(board, x, y+1) == player)));
+  return ((is_inb(i-1, j) && (get_cell(board, i-1, j) == player)) ||
+	  (is_inb(i+1, j) && (get_cell(board, i+1, j) == player)) ||
+	  (is_inb(i, j-1) && (get_cell(board, i, j-1) == player)) ||
+	  (is_inb(i, j+1) && (get_cell(board, i, j+1) == player)));
 }
 
 /** NAÏVE FUNCTION:
@@ -122,25 +122,25 @@ bool is_next_to_player(board_d* board, char player, int x, int y)
 */
 void update_board(board_d* board, char player, char color_input)
 {
-	int i, j;
-	bool has_changed = true;
-	
-	while (has_changed)
+  int i, j;
+  bool has_changed = true;
+  
+  while (has_changed)
+    {
+      has_changed = false;
+      
+      for (i = 0; i < BOARD_SIZE; i++)
 	{
-		has_changed = false;
-		
-		for (i = 0; i < BOARD_SIZE; i++)
+	  for (j = 0; j < BOARD_SIZE; j++)
+	    {
+	      if (is_next_to_player(board, player, i, j) && (get_cell(board, i, j) == color_input))
 		{
-			for (j = 0; j < BOARD_SIZE; j++)
-			{
-				if (is_next_to_player(board, player, i, j) && (get_cell(board, i, j) == color_input))
-				{
-					set_cell(board, i, j, player);
-					has_changed = true;
-				}
-			}
+		  set_cell(board, i, j, player);
+		  has_changed = true;
 		}
+	    }
 	}
+    }
 }
 
 void update_board_rec(board_d* board, board_d* board_visited, char player, char color_input, int i, int j)
