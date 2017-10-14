1c6784075cbc3e84cbd9580f510d40ec82dc74cd
Clement Legrand <clegrand@caraja.inrialpes.fr>
2017-10-14 08:48:57 +0200

    forgot to modify the file.h once again

--- proj_mod/IA.h
+++ proj_mod/IA.h
@@ -2,6 +2,8 @@
 #define IA_H
 
 int calc_new_cells(board_d* board, char player, char color);
+int calc_new_cells_rec(board_d* board, board_d* board_visited, char player, char color, int i, int j);
+int cal_new_cells_optimized(board_d* board, char player, char color, int i, int j);
 char IA_greedy(board_d* board);
 char IA_random(void);
 
