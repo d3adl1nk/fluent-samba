06a295c078f38bb1321c790a90fb37fe20ff9d91
d3adl1nk <marco.freire@ens-rennes.fr>
2017-10-16 18:51:39 +0200

    I broke up the main function into more manageable bits, indented and prettied up a big part of the code, in this state I think that it is readable by humans :P

--- proj_mod/board.c
+++ proj_mod/board.c
@@ -3,61 +3,56 @@
 
 
 board_d* board_create(void)
-{	
-  //char cells[BOARD_SIZE * BOARD_SIZE] = { 0 };
-  //board_d init_board = {{ 0 }, 1, 1};
-  //board_d* board = &init_board;
-  
-  board_d* board = malloc(sizeof(board_d));
-  (*board).num_cells_up = 0;
-  (*board).num_cells_down = 0;
-  
-  return board;
+{	 
+	board_d* board = malloc(sizeof(board_d));
+	(*board).num_cells_up = 0;
+	(*board).num_cells_down = 0;
+	
+	return board;
 }
 
 void board_free(board_d* board)
 {
-  free(board);
+	free(board);
 }
 
-/** Retrieves the color of a given board cell */
 char get_cell(board_d* board, int i, int j)
 {
-  return (*board).cells[i * BOARD_SIZE + j];
+	return (*board).cells[i * BOARD_SIZE + j];
 }
 
 int get_num_cells_up(board_d* board)
 {
-  return (*board).num_cells_up;
+	return (*board).num_cells_up;
 }
 
 int get_num_cells_down(board_d* board)
 {
-  return (*board).num_cells_down;
+	return (*board).num_cells_down;
 }
 
 /** Changes the color of a given board cell */
 void set_cell(board_d* board, int i, int j, char color)
 {
-  if (get_cell(board, i, j) == 'v')
-    {
-      (*board).num_cells_down -= 1;
-    }
-  if (get_cell(board, i, j) == '^')
-    {
-      (*board).num_cells_up -= 1;
-    }
-  
-  (*board).cells[i * BOARD_SIZE + j] = color;
-  if (color == 'v')
-    {
-      (*board).num_cells_down += 1;
-    }
-  if (color == '^')
-    {
-      (*board).num_cells_up += 1;
-    }
-  
+	if (get_cell(board, i, j) == 'v')
+	{
+		(*board).num_cells_down -= 1;
+	}
+	if (get_cell(board, i, j) == '^')
+	{
+		(*board).num_cells_up -= 1;
+	}
+	
+	(*board).cells[i * BOARD_SIZE + j] = color;
+	
+	if (color == 'v')
+	{
+		(*board).num_cells_down += 1;
+	}
+	if (color == '^')
+	{
+		(*board).num_cells_up += 1;
+	}
 }
 
 /** Prints the current state of the board on screen
@@ -67,52 +62,54 @@ void set_cell(board_d* board, int i, int j, char color)
  */
 void print_board(board_d* board)
 {
-  printf("\n");
+	printf("\n");
 	
-  int i, j;
-  for (i = 0; i < BOARD_SIZE; i++) {
-    for (j = 0; j < BOARD_SIZE; j++) {
-      printf("%c ", get_cell(board, i, j));
-    }
-    printf("\n");
-  }
-  printf("\n");
+	int i, j;
+	for (i = 0; i < BOARD_SIZE; i++)
+	{
+		for (j = 0; j < BOARD_SIZE; j++)
+		{
+			printf("%c ", get_cell(board, i, j));
+		}
+		printf("\n");
+	}
+	printf("\n");
 }
 
 /** Creates a random starting board, with a player on two opposite corners */
 void rand_board(board_d* board)
 {
-  srand(time(NULL));
-  char rand_color;
-  
-  int i, j;
-  for (i = 0; i < BOARD_SIZE; i++)
-    {
-      for (j = 0; j < BOARD_SIZE; j++)
+	srand(time(NULL));
+	char rand_color;
+	
+	int i, j;
+	for (i = 0; i < BOARD_SIZE; i++)
 	{
-	  rand_color = 65 + (rand() % 7);
-	  set_cell(board, i, j, rand_color);
+		for (j = 0; j < BOARD_SIZE; j++)
+		{
+			rand_color = 65 + (rand() % 7);
+			set_cell(board, i, j, rand_color);
+		}
 	}
-    }
-  
-  //initial state of both players
-  set_cell(board, BOARD_SIZE - 1, 0, 'v');
-  set_cell(board, 0, BOARD_SIZE - 1, '^');
+	
+	//initial state of both players
+	set_cell(board, BOARD_SIZE - 1, 0, 'v');
+	set_cell(board, 0, BOARD_SIZE - 1, '^');
 }
 
 /** Tells if a cell is inside the board */
 bool is_inb(int i, int j)
 {
-  return ((i >= 0 && i < BOARD_SIZE) && (j >= 0 && j < BOARD_SIZE));
+	return ((i >= 0 && i < BOARD_SIZE) && (j >= 0 && j < BOARD_SIZE));
 }
 
 /** Tells if a cell is next to a given player*/
 bool is_next_to_player(board_d* board, char player, int i, int j)
 {
-  return ((is_inb(i-1, j) && (get_cell(board, i-1, j) == player)) ||
-	  (is_inb(i+1, j) && (get_cell(board, i+1, j) == player)) ||
-	  (is_inb(i, j-1) && (get_cell(board, i, j-1) == player)) ||
-	  (is_inb(i, j+1) && (get_cell(board, i, j+1) == player)));
+	return ((is_inb(i-1, j) && (get_cell(board, i-1, j) == player)) ||
+		(is_inb(i+1, j) && (get_cell(board, i+1, j) == player)) ||
+		(is_inb(i, j-1) && (get_cell(board, i, j-1) == player)) ||
+		(is_inb(i, j+1) && (get_cell(board, i, j+1) == player)));
 }
 
 /** NAÏVE FUNCTION:
@@ -122,78 +119,85 @@ bool is_next_to_player(board_d* board, char player, int i, int j)
 */
 void update_board(board_d* board, char player, char color_input)
 {
-  int i, j;
-  bool has_changed = true;
-  
-  while (has_changed)
-    {
-      has_changed = false;
-      
-      for (i = 0; i < BOARD_SIZE; i++)
+	int i, j;
+	bool has_changed = true;
+
+	while (has_changed)
 	{
-	  for (j = 0; j < BOARD_SIZE; j++)
-	    {
-	      if (is_next_to_player(board, player, i, j) && (get_cell(board, i, j) == color_input))
+		has_changed = false;
+
+		for (i = 0; i < BOARD_SIZE; i++)
 		{
-		  set_cell(board, i, j, player);
-		  has_changed = true;
+			for (j = 0; j < BOARD_SIZE; j++)
+			{
+				if (is_next_to_player(board, player, i, j) && (get_cell(board, i, j) == color_input))
+				{
+					set_cell(board, i, j, player);
+					has_changed = true;
+				}
+			}
 		}
-	    }
 	}
-    }
 }
 
 void update_board_rec(board_d* board, board_d* board_visited, char player, char color_input, int i, int j)
 {
-  if (!(get_cell(board_visited,i,j)) && 
-      ( get_cell(board,i,j) == color_input ||
-	get_cell(board,i,j) == player) ) 
-    { 
-      set_cell(board, i, j, player);
-      set_cell(board_visited, i, j, 1);
-      if (is_inb(i+1,j))
-	{
-	  update_board_rec(board, board_visited, player, color_input, i+1, j);
+	bool is_valid_cell = (get_cell(board,i,j) == color_input || get_cell(board,i,j) == player);
+	if (!(get_cell(board_visited,i,j)) && is_valid_cell) 
+	{ 
+		set_cell(board, i, j, player);
+		set_cell(board_visited, i, j, 1);
+		
+		//calls recursively the adjacent cells to update the board from there
+		if (is_inb(i+1,j))
+		{
+			update_board_rec(board, board_visited, player, color_input, i+1, j);
+		}
+		if (is_inb (i-1, j))
+		{
+			update_board_rec(board, board_visited, player, color_input, i-1 ,j);
+		}
+		if (is_inb (i, j+1))
+		{
+			update_board_rec(board, board_visited, player, color_input, i, j+1);
+		}
+		if (is_inb (i, j-1))
+		{
+			update_board_rec(board, board_visited, player, color_input, i, j-1);
+		}
 	}
-      if (is_inb (i-1, j))
-	{
-	  update_board_rec(board, board_visited, player, color_input, i-1 ,j);
-	}
-      if (is_inb (i, j+1))
-	{
-	  update_board_rec(board, board_visited, player, color_input, i, j+1);
-	}
-      if (is_inb (i, j-1))
-	{
-	  update_board_rec(board, board_visited, player, color_input, i, j-1);
-	}
-    }
 }
 
 void update_board_optimized(board_d* board, char player, char color_input)
 {
-  int i, j;
-  board_d* board_visited = board_create(); 
-  if (player == '^')
-    {
-      i = 0;
-      j = BOARD_SIZE - 1;
-    }
-  else
-    {
-      i = BOARD_SIZE - 1;
-      j = 0;
-    }
-  int k, l;
-  for (k = 0; k < BOARD_SIZE; k++)
-    {
-      for (l = 0; l < BOARD_SIZE; l++)
+	int i, j;
+	board_d* board_visited = board_create(); 
+
+	//starting coordinates
+	if (player == '^')
 	{
-	  set_cell(board_visited, k, l, 0);  
+		i = 0;
+		j = BOARD_SIZE - 1;
 	}
-    }
-  update_board_rec(board, board_visited, player, color_input, i, j);
-  board_free(board_visited);
+	else
+	{
+		i = BOARD_SIZE - 1;
+		j = 0;
+	}
+
+	//board_visited initialization
+	int k, l;
+	for (k = 0; k < BOARD_SIZE; k++)
+	{
+		for (l = 0; l < BOARD_SIZE; l++)
+		{
+			set_cell(board_visited, k, l, 0);  
+		}
+	}
+
+	update_board_rec(board, board_visited, player, color_input, i, j);
+
+	board_free(board_visited);
 }
 
 
--- proj_mod/mainC.c
+++ proj_mod/mainC.c
@@ -1,78 +1,7 @@
-// NOTE: variables for a for loop must be defined before the loop
-// NOTE2 : by default, the player is '^' and the IA 'v'
-
-/* TODO:
- * - define constants for player characters?
- * - display color chosen by IA
- * - reduce main function into fragments
-*/
+// NOTE : by default, the player is '^' and the IA 'v'
 
 #include "board.h"
 #include "IA.h"
-
-/* Template of the 7 wonders of the world of the 7 colors assigment. */
-
-/** Represent the actual current board game
- *
- * NOTE: global variables are usually discouraged (plus encapsulation in
- *     an appropriate data structure would also be preferred), but don't worry.
- *     For this first assignment, no dinosaure will get you if you do that.
- */
-
-/*
-char board_visited[BOARD_SIZE * BOARD_SIZE] = { 0 }; // Filled with zeros, will be used for the third question !
-
-void update_board_rec(char player, char color_input, int i, int j)
-{
-  if (!(board_visited[i*BOARD_SIZE + j]) && 
-      (get_cell(i,j) == color_input || get_cell(i,j) == player)) 
-    { 
-      set_cell(i, j, player);
-      board_visited[i*BOARD_SIZE + j] = 1;
-      if (is_inb(i+1,j))
-	{
-	  update_board_rec(player, color_input, i+1, j);
-	}
-      if (is_inb (i-1, j))
-	{
-	  update_board_rec(player, color_input, i-1 ,j);
-	}
-      if (is_inb (i, j+1))
-	{
-	  update_board_rec(player, color_input, i, j+1);
-	}
-      if (is_inb (i, j-1))
-	{
-	  update_board_rec(player, color_input, i, j-1);
-	}
-    }
-}
-
-void update_board_optimized(char player, char color_input)
-{
-  int i, j;
-  if (player == '^')
-    {
-      i = 0;
-      j = BOARD_SIZE - 1;
-    }
-  else
-    {
-      i = BOARD_SIZE - 1;
-      j = 0;
-    }
-  int k, l;
-  for (k = 0; k < BOARD_SIZE; k++)
-    {
-      for (l = 0; l < BOARD_SIZE; l++)
-	{
-	  board_visited[k*BOARD_SIZE + l] = 0;  
-	}
-    }
-  update_board_rec(player, color_input, i, j);
-}
-*/
-
 
 /** TEST FUNCTION: iterates sthg on every cell of the board */
 /*
@@ -109,115 +38,99 @@ char player_input(void)
 	return color_input;
 }
 
-/* Updates the number of cells each player owns */
-/* void update_num_cells(board_d* board, char player) */
-/* { */
-/* 	if (player == '^') */
-/* 	{ */
-/* 		(*board).num_cells_up = 0; */
-/* 	} */
-/* 	else if (player == 'v') */
-/* 	{ */
-/* 		(*board).num_cells_down = 0; */
-/* 	} */
-	
-/* 	int i, j; */
-/* 	for (i = 0; i < BOARD_SIZE; i++) */
-/* 	{ */
-/* 		for (j = 0; j < BOARD_SIZE; j++) */
-/* 		{ */
-/* 			if ((player == '^') && (get_cell(board, i, j) == '^')) */
-/* 			{ */
-/* 				(*board).num_cells_up++; */
-/* 			} */
-/* 			else if ((player == 'v') && (get_cell(board, i, j) == 'v')) */
-/* 			{ */
-/* 				(*board).num_cells_down++; */
-/* 			} */
-/* 		} */
-/* 	} */
-/* } */
-
 /** Prints the percentage of the board each player owns */
 void print_occupation(board_d* board)
 {
-  int perc_up = 100 * get_num_cells_up(board) / (BOARD_SIZE*BOARD_SIZE);
-  int perc_down = 100 * get_num_cells_down(board) / (BOARD_SIZE*BOARD_SIZE);
+	int perc_up = 100 * get_num_cells_up(board) / (BOARD_SIZE*BOARD_SIZE);
+	int perc_down = 100 * get_num_cells_down(board) / (BOARD_SIZE*BOARD_SIZE);
 
-	printf("PLAYER ^: %d\nPLAYER v: %d\n", perc_up, perc_down);
+	printf("OCCUPATION:\nPLAYER ^: %d\nPLAYER v: %d\n", perc_up, perc_down);
 	printf("###########################################################\n");
 }
 
 /** Checks if the victory condition is met */
 bool is_end(board_d* board)
 {	
-  int perc_up = 100 * get_num_cells_up(board) / (BOARD_SIZE*BOARD_SIZE);
-  int perc_down = 100 * get_num_cells_down(board) / (BOARD_SIZE*BOARD_SIZE);
+	int perc_up = 100 * get_num_cells_up(board) / (BOARD_SIZE*BOARD_SIZE);
+	int perc_down = 100 * get_num_cells_down(board) / (BOARD_SIZE*BOARD_SIZE);
 	
-  return ((perc_up > 50) || (perc_down > 50));
+	return ((perc_up > 50) || (perc_down > 50));
 }
 
 /**Given a player, returns the other one*/
 char change_player(char curr_player)
 {
-  return (curr_player == '^') ? 'v' : '^';
+	return (curr_player == '^') ? 'v' : '^';
+}
+
+void game_init(board_d* board)
+{
+	printf("\n\nWelcome to the 7 wonders of the world of the 7 colors\n"
+			"*****************************************************\n\n"
+			"Current board state:\n");
+	
+	rand_board(board);
+	print_board(board);
+	print_occupation(board);
+}
+
+void game_loop(board_d* board, char* player, bool* is_IA_turn)
+{
+	char color_input;
+	
+  	while (!is_end(board))
+	{
+		switch (*player)
+		{
+			case '^':
+				color_input = player_input();
+				*is_IA_turn = false;
+				break;
+	  
+			case 'v':
+				color_input = IA_random();
+				*is_IA_turn = true;
+				break;
+	  
+			default:
+				printf("\nERROR\n");
+		}			
+		update_board_optimized(board, *player, color_input);
+	  
+		if (*is_IA_turn)
+		{
+			printf("\nTHE IA HAS CHOSEN THIS COLOR: %c\n", color_input);
+			print_board(board);
+		}
+	  
+		print_occupation(board);
+	  
+		*player = change_player(*player);
+	}
+}
+
+void game_end(board_d* board, char* player)
+{
+	*player = change_player(*player);
+	
+	print_board(board);
+	printf("###########################################################\n");
+	printf("\nPLAYER %c WON!\n\n", *player);
+	board_free(board);
 }
 
 /** Program entry point */
 int main(void)
 {
-  printf("\n\nWelcome to the 7 wonders of the world of the 7 colors\n"
-	 "*****************************************************\n\n"
-	 "Current board state:\n");
-    
-  char color_input;
-  char player = '^';
-  
-  board_d* board;
-  board = board_create();
-  
-  bool is_IA_turn = false;
-  
-  rand_board(board);
-  print_board(board);
-  print_occupation(board);
-  
-  while (!is_end(board))
-    {
-      switch (player)
-	{
-	case '^':
-	  color_input = player_input();
-	  is_IA_turn = false;
-	  break;
-	  
-	case 'v':
-	  color_input = IA_foresighted_greedy(board, player);
-	  is_IA_turn = true;
-	  break;
-	  
-	default:
-	  printf("\nERROR\n");
-	}			
-      
-      update_board_optimized(board, player, color_input);
-      
-      if (is_IA_turn)
-	{
-	  printf("\nTHE IA HAS CHOSEN THIS COLOR: %c\n", color_input);
-			print_board(board);
-	}
-      
-      /* update_num_cells(board, player); */
-      printf("occupation:");
-      print_occupation(board);
-      
-      player = change_player(player);
-    }
-  
-  print_board(board);
-  
-  //free(board);
+	board_d* board = board_create();
+	char player[1] = "^";
+	
+	bool does_IA_start = false;
+	bool* is_IA_turn = &does_IA_start;
+	
+	game_init(board);
+	game_loop(board, player, is_IA_turn);
+	game_end(board, player);
   
   return 0; // Everything went well
 }
